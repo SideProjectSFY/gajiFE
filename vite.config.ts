9a1abe6d81ca9a194f104cf012e701c918818f22
yeomin4242 <whereismysejong@naver.com>
2025-12-22 14:12:01 +0900

    feat: update Vite configuration to include template compiler options for Vue plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { templateCompilerOptions } from '@tresjs/core'
 import svgLoader from 'vite-svg-loader'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      ...templateCompilerOptions,
+    }),
     svgLoader({
       defaultImport: 'component',
       svgoConfig: {
